4ea78de8a8762e6f3e88384fb04f676bb3d63bc9
DvrDania <dvr.dania@gmail.com>
2021-11-08 15:02:38 +0330

    getting ingredients

--- src/public.rs
+++ src/public.rs
@@ -1,4 +1,21 @@
+use crate::schema::ingredients;
+use crate::{database, IngredientCategory};
+use diesel::prelude::*;
+
+#[derive(Queryable, Debug)]
+struct Ingredient {
+    id: i32,
+    name: String,
+    amount: i32,
+    category: IngredientCategory,
+    price: f32,
+}
+
 #[get("/ingredients")]
 pub fn get_ingredients() {
-    println!("Gets ingredients list");
+    let conn = database::establish_connection();
+    let results = ingredients::table
+        .load::<Ingredient>(&conn)
+        .expect("Error getting ingredients");
+    println!("{:#?}", results);
 }
