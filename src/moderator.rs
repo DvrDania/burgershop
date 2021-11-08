15a5904d137cb53fa7e520018f5ab7cd4d62d379
DvrDania <dvr.dania@gmail.com>
2021-11-08 15:01:45 +0330

    more consistency

--- src/moderator.rs
+++ src/moderator.rs
@@ -6,11 +6,11 @@ use rocket::serde::json::Json;
 
 #[post("/ingredients", data = "<items>", format = "json")]
 pub fn fill_ingredients(items: Json<Vec<Ingredient>>) {
-    let connection = database::establish_connection();
+    let conn = database::establish_connection();
     let items = items.into_inner();
 
     diesel::insert_into(ingredients::table)
         .values(&items)
-        .execute(&connection)
+        .execute(&conn)
         .expect("Error saving ingredients");
 }
