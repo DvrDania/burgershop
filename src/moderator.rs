3940734d3cd4cf7f80893aa31c2f5fffcf549ef1
DvrDania <dvr.dania@gmail.com>
2021-11-09 20:01:27 +0330

    refactored + proper response for set_ingredients fn

--- src/moderator.rs
+++ src/moderator.rs
@@ -1,11 +1,14 @@
-use crate::database;
-use crate::schema::ingredients;
+use crate::api_response::ApiResponse;
+use crate::database::{self, ingredients};
 use crate::Ingredient;
 use diesel::prelude::*;
+use rocket::http::{ContentType, Status};
 use rocket::serde::json::Json;
 
 #[post("/ingredients", data = "<items>", format = "json")]
-pub fn fill_ingredients(items: Json<Vec<Ingredient>>) {
+pub fn set_ingredients(
+    items: Json<Vec<Ingredient>>,
+) -> (Status, (ContentType, Json<ApiResponse<()>>)) {
     let conn = database::establish_connection();
     let items = items.into_inner();
 
@@ -13,4 +16,10 @@ pub fn fill_ingredients(items: Json<Vec<Ingredient>>) {
         .values(&items)
         .execute(&conn)
         .expect("Error saving ingredients");
+    let res: ApiResponse<()> = ApiResponse {
+        success: true,
+        message: String::from("Ingredients set successfully"),
+        data: None,
+    };
+    (Status::Created, (ContentType::JSON, Json(res)))
 }
